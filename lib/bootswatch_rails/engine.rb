4b76970bfb2fdbee81918400f83ca935a36c8f88
Volker Wiegand <volker.wiegand@cvw.de>
2014-06-05 17:49:12 +0200

    added vendor files

--- lib/bootswatch_rails/engine.rb
+++ lib/bootswatch_rails/engine.rb
@@ -1,7 +1,7 @@
 module BootswatchRails
   class Engine < Rails::Engine
     initializer "BootswatchRails themes" do |app|
-      app.config.assets.precompile += %w()
+      app.config.assets.precompile += %w(amelia.css cerulean.css cosmo.css custom.css cyborg.css darkly.css flatly.css journal.css lumen.css readable.css simplex.css slate.css spacelab.css superhero.css united.css yeti.css)
       app.config.assets.paths << File.expand_path('../../../vendor/assets/fonts', __FILE__)
     end
   end
