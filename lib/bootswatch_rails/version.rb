41173f53548c24287e9d1ab9695566be6b1361d6
Volker Wiegand <volker.wiegand@cvw.de>
2014-06-18 15:08:33 +0200

    added en locale; display brand only if brand_logo non-empty

--- lib/bootswatch_rails/version.rb
+++ lib/bootswatch_rails/version.rb
@@ -1,5 +1,5 @@
 module BootswatchRails
-  VERSION = "3.1.1.14"
+  VERSION = "3.1.1.15"
   THEMES = [:amelia, :cerulean, :cosmo, :custom, :cyborg, :darkly, :flatly, :journal, :lumen, :readable, :simplex, :slate, :spacelab, :superhero, :united, :yeti]
   DEFAULT = 1
 end
