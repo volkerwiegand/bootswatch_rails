bc95b381609a568745c3cb2af439f222421eca12
Volker Wiegand <volker.wiegand@cvw.de>
2014-06-18 15:37:32 +0200

    make email link clickable (mailto:...)

--- lib/bootswatch_rails/version.rb
+++ lib/bootswatch_rails/version.rb
@@ -1,5 +1,5 @@
 module BootswatchRails
-  VERSION = "3.1.1.15"
+  VERSION = "3.1.1.16"
   THEMES = [:amelia, :cerulean, :cosmo, :custom, :cyborg, :darkly, :flatly, :journal, :lumen, :readable, :simplex, :slate, :spacelab, :superhero, :united, :yeti]
   DEFAULT = 1
 end
