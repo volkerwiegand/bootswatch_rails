f9296a76c35e57f74d54b37a8f376361a3d0acf3
Volker Wiegand <volker.wiegand@cvw.de>
2014-08-19 21:45:16 +0200

    make turbolinks optional (prevents autofocus in some cases)

--- lib/bootswatch_rails/version.rb
+++ lib/bootswatch_rails/version.rb
@@ -1,5 +1,5 @@
 module BootswatchRails
-  VERSION = "3.2.0.2"
+  VERSION = "3.2.0.3"
   THEMES = [:amelia, :cerulean, :cosmo, :custom, :cyborg, :darkly, :flatly, :journal, :lumen, :paper, :readable, :sandstone, :simplex, :slate, :spacelab, :superhero, :united, :yeti]
   DEFAULT = 1
 end
--- lib/generators/bootswatch_rails/install/install_generator.rb
+++ lib/generators/bootswatch_rails/install/install_generator.rb
@@ -4,12 +4,18 @@ module BootswatchRails
       desc "Setup application to use bootswatch.com"
       class_option :bootstrap, type: :boolean, default: true,
                desc: 'Add bootstrap to application.js'
+      class_option :turbolinks, type: :boolean, default: false,
+               desc: 'Activate turbolinks (off by default)'
       source_root File.expand_path("../templates", __FILE__)
       
       def update_javascripts
         return unless options.bootstrap?
         file = "app/assets/javascripts/application.js"
         inject_into_file file, "\n//= require bootstrap", after: /require jquery_ujs$/
+        unless options.turbolinks?
+          gsub_file file, /^\/\/(= require turbolinks)/, "//---\\1"
+          comment_lines "Gemfile", /gem 'turbolinks/
+        end
       end
       
       def update_application_controller
@@ -36,6 +42,12 @@ module BootswatchRails
         directory "app", force: true
         directory "config"
         directory "lib", force: true
+      end
+      
+      def remove_turbolinks
+        return if options.turbolinks?
+        file = "app/views/layouts/_head.html.erb"
+        gsub_file file, /, 'data-turbolinks-track' => true/, ""
       end
     end
   end
