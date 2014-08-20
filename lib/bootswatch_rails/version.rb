ab7b515ff22c400c95f102c31061b6d416c63a5d
Volker Wiegand <volker.wiegand@cvw.de>
2014-08-20 03:41:02 +0200

    added cleditor_generator

--- lib/bootswatch_rails/version.rb
+++ lib/bootswatch_rails/version.rb
@@ -1,5 +1,5 @@
 module BootswatchRails
-  VERSION = "3.2.0.5"
+  VERSION = "3.2.0.6"
   THEMES = [:amelia, :cerulean, :cosmo, :custom, :cyborg, :darkly, :flatly, :journal, :lumen, :paper, :readable, :sandstone, :simplex, :slate, :spacelab, :superhero, :united, :yeti]
   DEFAULT = 1
 end
---
+++ lib/generators/bootswatch_rails/cleditor/cleditor_generator.rb
@@ -0,0 +1,29 @@
+require 'rails/generators/active_record'
+
+module BootswatchRails
+  module Generators
+    class CleditorGenerator < ActiveRecord::Generators::Base
+      desc "Turn text_area(s) into rich edit fields with CLEditor"
+      argument :name, type: :string,
+               desc: "The resource with textareas to be enhanced"
+      argument :columns, type: :array, banner: "COLUMN [...]",
+               desc: "The names of the textarea columns"
+      source_root File.expand_path('../templates', __FILE__)
+      
+      def add_to_assets
+        file = "app/assets/javascripts/application.js"
+        inject_into_file file, "\n//= require jquery.cleditor", after: /require jquery_ujs$/
+        file = "app/assets/stylesheets/application.css"
+        inject_into_file file, " *= require jquery.cleditor\n", before: /^.*require_self/
+      end
+      
+      def add_to_view
+        file = "app/views/#{table_name}/_form.html.erb"
+        columns.each do |column|
+          gsub_file file, /(input :#{column}) /, "\\1, input_html: { class: 'cleditor' } "
+        end
+      end
+    end
+  end
+end
+
