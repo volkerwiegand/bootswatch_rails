940620eee09958c9bfbe6152189dc7da3023efca
Volker Wiegand <volker.wiegand@cvw.de>
2014-08-22 23:09:52 +0200

    update _head.html.erb

--- lib/generators/bootswatch_rails/install/install_generator.rb
+++ lib/generators/bootswatch_rails/install/install_generator.rb
@@ -2,21 +2,9 @@ module BootswatchRails
   module Generators
     class InstallGenerator < Rails::Generators::Base
       desc "Setup application to use bootswatch.com"
-      class_option :bootstrap, type: :boolean, default: false,
-               desc: 'Add bootstrap to application.js'
       class_option :turbolinks, type: :boolean, default: false,
                desc: 'Activate turbolinks (off by default)'
       source_root File.expand_path("../templates", __FILE__)
-      
-      def update_javascripts
-        return unless options.bootstrap?
-        file = "app/assets/javascripts/application.js"
-        inject_into_file file, "\n//= require bootstrap", after: /require jquery_ujs$/
-        unless options.turbolinks?
-          gsub_file file, /^\/\/= require turbolinks\n/, ""
-          comment_lines "Gemfile", /gem 'turbolinks/
-        end
-      end
       
       def update_application_controller
         file = "app/controllers/application_controller.rb"
@@ -42,10 +30,14 @@ module BootswatchRails
         directory "app", force: true
         directory "config"
         directory "lib", force: true
+        template "head.html.erb", "app/views/layouts/_head.html.erb"
       end
       
       def remove_turbolinks
         return if options.turbolinks?
+        comment_lines "Gemfile", /gem 'turbolinks/
+        file = "app/assets/javascripts/application.js"
+        gsub_file file, /^\/\/= require turbolinks/, ""
         file = "app/views/layouts/_head.html.erb"
         gsub_file file, /, 'data-turbolinks-track' => true/, ""
       end
