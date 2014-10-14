bf342385ab4cd3a37a9f84b158f46bd92d0a55ee
Volker Wiegand <volker.wiegand@cvw.de>
2014-10-14 08:56:22 +0200

    added pre and post hooks for content_for head

--- lib/bootswatch_rails/version.rb
+++ lib/bootswatch_rails/version.rb
@@ -1,6 +1,6 @@
 module BootswatchRails
   BOOTSTRAP = "3.2.0"
-  VERSION = "3.2.0.25"
+  VERSION = "3.2.0.26"
   THEMES = [:amelia, :cerulean, :cosmo, :custom, :cyborg, :darkly, :flatly, :journal, :lumen, :paper, :readable, :sandstone, :simplex, :slate, :spacelab, :superhero, :united, :yeti]
   DEFAULT = 1
 end
