1a15dc41fa66facc73252ca8903fb11fa8b65ee0
Volker Wiegand <volker.wiegand@cvw.de>
2014-10-20 05:38:00 +0200

    added page_title helper

--- lib/generators/bootswatch_rails/install/templates/app/helpers/application_helper.rb
+++ lib/generators/bootswatch_rails/install/templates/app/helpers/application_helper.rb
@@ -2,19 +2,23 @@ module ApplicationHelper
   def app_name
     "Demo Application"
   end
-  
+
+  def page_title(title_text)
+    content_for(:title) { "#{title_text} - #{app_name}" }
+  end
+
   def brand_logo
     image_tag("Rails_logo_80.jpg")
   end
-  
+
   def copyright_year
     Time.now.year
   end
-  
+
   def copyright_text
     "Copyright Holder"
   end
-  
+
   def copyright_link
     "http://www.example.com/"
   end
