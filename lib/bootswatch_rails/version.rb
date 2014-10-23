f47f901e1131b56a014154c20ec93b0e147a269e
Volker Wiegand <volker.wiegand@cvw.de>
2014-10-23 04:53:14 +0200

    added gravatar.com image

--- lib/bootswatch_rails/version.rb
+++ lib/bootswatch_rails/version.rb
@@ -1,6 +1,6 @@
 module BootswatchRails
   BOOTSTRAP = "3.2.0"
-  VERSION = "3.2.0.33"
+  VERSION = "3.2.0.34"
   FONT_AWESOME = "4.2.0"
 
   THEMES = [:amelia, :cerulean, :cosmo, :custom, :cyborg, :darkly, :flatly, :journal, :lumen, :paper, :readable, :sandstone, :simplex, :slate, :spacelab, :superhero, :united, :yeti]
