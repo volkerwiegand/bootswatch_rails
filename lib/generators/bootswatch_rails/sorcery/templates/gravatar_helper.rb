a95138f48835b95a1e10a151dc9499d268b01c76
Volker Wiegand <volker.wiegand@cvw.de>
2014-10-23 04:23:23 +0200

    added gravatar.com image

--- lib/generators/bootswatch_rails/sorcery/templates/gravatar_helper.rb
+++ lib/generators/bootswatch_rails/sorcery/templates/gravatar_helper.rb
@@ -1,6 +1,6 @@
 module GravatarHelper
   def gravatar_url(<%= name %>, size)
     gravatar_id = Digest::MD5::hexdigest(<%= name %>.email).downcase
-    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=mm"
   end
 end
