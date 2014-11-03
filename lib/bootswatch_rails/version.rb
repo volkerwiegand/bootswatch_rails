cf304767d3666d80ba7bda6c91d3c32ff1953598
Volker Wiegand <volker.wiegand@cvw.de>
2014-11-03 01:29:40 +0100

    bugfix in engine.rb: use Ruby variable, not ERB

--- lib/bootswatch_rails/version.rb
+++ lib/bootswatch_rails/version.rb
@@ -1,7 +1,7 @@
 module BootswatchRails
   BOOTSTRAP = "3.3.0"
   FONT_AWESOME = "4.2.0"
-  VERSION = "3.3.0.2"
+  VERSION = "3.3.0.3"
 
   THEMES = [:cerulean, :cosmo, :custom, :cyborg, :darkly, :flatly, :journal, :lumen, :paper, :readable, :sandstone, :simplex, :slate, :spacelab, :superhero, :united, :yeti]
   DEFAULT = 0
