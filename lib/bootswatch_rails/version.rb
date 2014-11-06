09ff5e864873ca6adb2a6d5cff7de748fc10b4e0
Volker Wiegand <volker.wiegand@cvw.de>
2014-11-06 12:42:24 +0100

    updated bootswatch CDN to version 3.3.0

--- lib/bootswatch_rails/version.rb
+++ lib/bootswatch_rails/version.rb
@@ -1,8 +1,8 @@
 module BootswatchRails
   BOOTSTRAP = "3.3.0"
-  BOOTSWATCH = "3.2.0"
+  BOOTSWATCH = "3.3.0"
   FONT_AWESOME = "4.2.0"
-  VERSION = "3.3.0.6"
+  VERSION = "3.3.0.7"
 
   THEMES = [:cerulean, :cosmo, :custom, :cyborg, :darkly, :flatly, :journal, :lumen, :paper, :readable, :sandstone, :simplex, :slate, :spacelab, :superhero, :united, :yeti]
   DEFAULT = 0
