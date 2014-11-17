ffb95e72e442e07cd82bb216e9ba518bcdae3af7
Volker Wiegand <volker.wiegand@cvw.de>
2014-11-17 11:22:15 +0100

    updated bootswatch to version 3.3.1

--- lib/bootswatch_rails/version.rb
+++ lib/bootswatch_rails/version.rb
@@ -1,9 +1,9 @@
 module BootswatchRails
   BOOTSTRAP = "3.3.1"
-  BOOTSWATCH = "3.3.0"
+  BOOTSWATCH = "3.3.1"
   FONT_AWESOME = "4.2.0"
-  VERSION = "3.3.0.11"
+  VERSION = "3.3.1.0"
 
-  THEMES = [:cerulean, :cosmo, :cyborg, :darkly, :flatly, :journal, :lumen, :paper, :readable, :sandstone, :simplex, :slate, :spacelab, :superhero, :united, :yeti]
+  THEMES = [:cerulean, :cosmo, :custom, :cyborg, :darkly, :flatly, :journal, :lumen, :paper, :readable, :sandstone, :simplex, :slate, :spacelab, :superhero, :united, :yeti]
   DEFAULT = 0
 end
