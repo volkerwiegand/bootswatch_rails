c8925b2c5d43785ba3a2f1db53786198819d3805
Volker Wiegand <volker.wiegand@cvw.de>
2014-11-18 21:57:10 +0100

    added jquery.dataTables.css to asset pipeline

--- lib/bootswatch_rails/engine.rb
+++ lib/bootswatch_rails/engine.rb
@@ -40,7 +40,7 @@ module BootswatchRails
       ActiveSupport.on_load(:action_view) do
         include BootswatchRails::ActionViewExtensions
       end
-      app.config.assets.precompile += %w(cerulean.css cosmo.css custom.css cyborg.css darkly.css flatly.css journal.css lumen.css paper.css readable.css sandstone.css simplex.css slate.css spacelab.css superhero.css united.css yeti.css)
+      app.config.assets.precompile += %w(jquery.dataTables.css cerulean.css cosmo.css custom.css cyborg.css darkly.css flatly.css journal.css lumen.css paper.css readable.css sandstone.css simplex.css slate.css spacelab.css superhero.css united.css yeti.css)
       app.config.assets.paths << File.expand_path('../../../vendor/assets/fonts', __FILE__)
     end
   end
