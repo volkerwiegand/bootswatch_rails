c20ac0be10abfc76b594fc6d8de41b98b10e4691
Volker Wiegand <volker.wiegand@cvw.de>
2014-12-07 17:42:31 +0100

    bump tiny version number

--- lib/bootswatch_rails/version.rb
+++ lib/bootswatch_rails/version.rb
@@ -4,7 +4,7 @@ module BootswatchRails
   FONT_AWESOME = "4.2.0"
   DATATABLES = "1.10.4"
   RESPONSIVE = "1.0.2"
-  VERSION = "3.3.1.1"
+  VERSION = "3.3.1.2"
 
   THEMES = [:cerulean, :cosmo, :custom, :cyborg, :darkly, :flatly, :journal, :lumen, :paper, :readable, :sandstone, :simplex, :slate, :spacelab, :superhero, :united, :yeti]
   DEFAULT = 0
