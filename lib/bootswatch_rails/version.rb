e688c64dc243a811a0fdc03d9dff9accd2562207
Volker Wiegand <volker.wiegand@cvw.de>
2015-01-20 19:06:18 +0100

    updated libraries

--- lib/bootswatch_rails/version.rb
+++ lib/bootswatch_rails/version.rb
@@ -1,10 +1,10 @@
 module BootswatchRails
-  BOOTSTRAP = "3.3.1"
-  BOOTSWATCH = "3.3.0"
+  BOOTSTRAP = "3.3.2"
+  BOOTSWATCH = "3.3.1"
   FONT_AWESOME = "4.2.0"
   DATATABLES = "1.10.4"
   RESPONSIVE = "1.0.2"
-  VERSION = "3.3.1.2"
+  VERSION = "3.3.2.1"
 
   THEMES = [:cerulean, :cosmo, :custom, :cyborg, :darkly, :flatly, :journal, :lumen, :paper, :readable, :sandstone, :simplex, :slate, :spacelab, :superhero, :united, :yeti]
   DEFAULT = 0
