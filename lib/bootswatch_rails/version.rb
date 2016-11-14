51a0db87a372fb869097662c3b6fab19a5837a9e
Volker Wiegand <volker@railduino.de>
2016-11-14 06:21:38 +0100

    added application layout (single, sidebar, even); added theme selector

--- lib/bootswatch_rails/version.rb
+++ lib/bootswatch_rails/version.rb
@@ -4,7 +4,7 @@ module BootswatchRails
   FONT_AWESOME = "4.6.2"
   DATATABLES = "1.10.12"
   RESPONSIVE = "2.1.0"
-  VERSION = "3.3.7.2"
+  VERSION = "3.3.7.3"
 
   THEMES = [:cerulean, :cosmo, :custom, :cyborg, :darkly, :flatly, :journal, :lumen, :paper, :readable, :sandstone, :simplex, :slate, :spacelab, :superhero, :united, :yeti]
   DEFAULT = 0
--- lib/generators/bootswatch_rails/install/install_generator.rb
+++ lib/generators/bootswatch_rails/install/install_generator.rb
@@ -11,12 +11,18 @@ module BootswatchRails
       class_option :cdn, type: :string, default: 'none',
                banner: 'none, google, microsoft, jquery or yandex',
                desc: 'Use CDN (requires jquery[-ui]-rails-cdn gems)'
+      class_option :devise, type: :boolean, default: false,
+               desc: 'Call user_signed_in? instead of logged_in?'
+      class_option :layout, type: :string, default: 'single',
+               banner: 'single, sidebar or even',
+               desc: 'Setup application layout (default single=12-col)'
       source_root File.expand_path("../templates", __FILE__)
 
       def update_application_controller
         file = "app/controllers/application_controller.rb"
         inject_into_file file, "\n\n  private", after: /protect_from_forgery.*$/
 
+        xline = "    ?@current_theme = current_user.theme if #{auth_check}"
         lines = [
           "",
           "  def default_theme",
@@ -25,7 +31,8 @@ module BootswatchRails
           "  helper_method :default_theme",
           "",
           "  def current_theme",
-          "    @current_theme = current_user.theme if logged_in?",
+          "    !@current_theme = current_user.theme if #{auth_check}",
+          xline,
           "    @current_theme ||= default_theme",
           "  end",
           "  helper_method :current_theme",
@@ -35,29 +42,39 @@ module BootswatchRails
       end
 
       def update_application_js
-        return if options.cdn != "none"
         file = "app/assets/javascripts/application.js"
-        inject_into_file file, after: /require jquery_ujs$/ do
-          "\n//= require bootstrap"
+        unless options.turbolinks?
+          gsub_file file, /\/\/= require turbolinks\n/, ''
         end
-        if options.ui?
-          inject_into_file file, after: /require jquery$/ do
-            "\n//= require jquery-ui"
+        if options.cdn == "none"
+          inject_into_file file, after: /require jquery_ujs$/ do
+            "\n//= require bootstrap"
           end
-        end
-        if options.dt?
-          inject_into_file file, before: /^\/\/= require_tree.*$/ do
-            "//= require jquery.dataTables\n" +
-            "//= require dataTables.responsive\n"
+          if options.ui?
+            inject_into_file file, after: /require jquery$/ do
+              "\n//= require jquery-ui"
+            end
+          end
+          if options.dt?
+            inject_into_file file, before: /^\/\/= require_tree.*$/ do
+              "//= require jquery.dataTables\n" +
+              "//= require dataTables.responsive\n"
+            end
           end
         end
       end
 
-      #def copy_application_js
-      #  file = "app/assets/javascripts/application.js"
-      #  remove_file file
-      #  template "application.js", file
-      #end
+      def update_application_css
+        if options.cdn == "none"
+          file = "app/assets/stylesheets/application.css"
+          if options.dt?
+            inject_into_file file, before: /^.*require_self$/ do
+              " *= jquery.dataTables\n" +
+              " *= responsive.dataTables\n"
+            end
+          end
+        end
+      end
 
       def setup_assets_precompile
         return if options.cdn == "none"
@@ -80,7 +97,18 @@ module BootswatchRails
         template "head.html.erb", "app/views/layouts/_head.html.erb"
       end
 
+      def setup_layout
+        file = "app/views/layouts/application.html.erb"
+        remove_file file
+        template "#{options.layout}.html.erb", file
+        template "theme.html.erb", "app/views/layouts/_theme.html.erb"
+      end
+
       protected
+
+      def auth_check
+        options.devise? ? "user_signed_in?" : "logged_in?"
+      end
 
       def turbolinks
         options.turbolinks? ? ", 'data-turbolinks-track' => true" : ""
